7005ebbbb2ff8dc6c20bf591074f23c97a70068c
Norbert <norbert.tovt.pz.2022@lpnu.ua>
2023-11-28 10:20:50 +0200

    fixed adding transaction bag

--- teamwork-frontend/src/main/java/com/norbert/frontend/service/JsonRequestBuilder.java
+++ teamwork-frontend/src/main/java/com/norbert/frontend/service/JsonRequestBuilder.java
@@ -1,13 +1,11 @@
 package com.norbert.frontend.service;
-
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.norbert.frontend.entity.Employee;
 import com.norbert.frontend.entity.Transaction;
 
 public class JsonRequestBuilder {
-    public static String buildTransactionJson(Transaction transaction) {
+    public static JsonObject buildTransactionJson(Transaction transaction) {
         JsonObject jsonRequest = new JsonObject();
         jsonRequest.addProperty("date", transaction.getDate().toString());
         jsonRequest.addProperty("order_type", transaction.getOrderType().name());
@@ -16,17 +14,17 @@ public class JsonRequestBuilder {
             employeesArray.add(buildEmployeeJson(employee));
         }
         jsonRequest.add("employees", employeesArray);
-        return new Gson().toJson(jsonRequest);
+        return jsonRequest;
     }
 
-    public static String buildEmployeeJson(Employee employee) {
-        JsonObject jsonRequest = new JsonObject();
-        if(employee.getId() != null)
-            jsonRequest.addProperty("id",employee.getId());
-        jsonRequest.addProperty("first_name", employee.getFirstName());
-        jsonRequest.addProperty("last_name", employee.getLastName());
-        jsonRequest.addProperty("card_number",employee.getCardNumber());
-        jsonRequest.addProperty("email",employee.getEmail());
-        return new Gson().toJson(jsonRequest);
+    public static JsonObject buildEmployeeJson(Employee employee) {
+        JsonObject employeeObject = new JsonObject();
+        if (employee.getId() != null)
+            employeeObject.addProperty("id", employee.getId());
+        employeeObject.addProperty("first_name", employee.getFirstName());
+        employeeObject.addProperty("last_name", employee.getLastName());
+        employeeObject.addProperty("card_number", employee.getCardNumber());
+        employeeObject.addProperty("email", employee.getEmail());
+        return employeeObject;
     }
 }
